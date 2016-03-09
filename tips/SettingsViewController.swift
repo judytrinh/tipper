1567797e434bbc327960c8f79ade669d3356244f
Judy Trinh <jtrinh@zynga.com>
2016-03-08 21:39:02 -0800

    Adding finished tip calculator project, update gitignore for DS_Store

---
+++ tips/SettingsViewController.swift
@@ -0,0 +1,54 @@
+//
+//  SettingsViewController.swift
+//  tips
+//
+//  Created by Judy Trinh on 3/7/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var defaultTipControl: UISegmentedControl!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func popSettingsView(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let index = defaults.integerForKey("defaultTipIndex")
+        defaultTipControl.selectedSegmentIndex = index
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setInteger(defaultTipControl.selectedSegmentIndex, forKey: "defaultTipIndex")
+        defaults.synchronize()
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
